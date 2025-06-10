6baf92b34255437a425ebabb23de6058feb6a989
BayuMahardika <114842921+Bayu-cyber-coder@users.noreply.github.com>
2025-06-10 14:51:24 +0800

    Update Request.java

--- src/server/Request.java
+++ src/server/Request.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-dpublic class Request {
+public class Request {
     private String method;
     private String path;
     private String body = "";
