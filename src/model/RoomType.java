17464fb9de049c75a487c8083cc15f8248936a7f
jihancacing <jiieahn@gmail.com>
2025-06-10 14:07:41 +0800

    model data strukturnya

---
+++ src/model/RoomType.java
@@ -0,0 +1,20 @@
+package model;
+
+public class RoomType {
+    public int id;
+    public int villa;
+    public String name;
+    public int quantity;
+    public int capacity;
+    public int price;
+    public String bed_size;
+    public boolean has_desk;
+    public boolean has_ac;
+    public boolean has_tv;
+    public boolean has_wifi;
+    public boolean has_shower;
+    public boolean has_hotwater;
+    public boolean has_fridge;
+
+    public RoomType() {}
+}
