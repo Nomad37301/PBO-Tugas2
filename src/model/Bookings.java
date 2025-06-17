93c32388ffc9cd2ae1ae1b8ffd60ed63528a7694
jihancacing <jiieahn@gmail.com>
2025-06-17 12:17:22 +0800

    Update tipe data agar lebih efisien

--- src/model/Bookings.java
+++ src/model/Bookings.java
@@ -7,10 +7,11 @@ public class Bookings {
     public String checkin_date; // sesuaikan tipe datanya / ni tipe data masih belum tentu
     public String checkout_date; // sesuaikan tipe datanya yaw / sama aja kek di atas
     public int price;
+    public int voucher;
     public int final_price;
     public String payment_status;
-    public int has_checkin = 0;
-    public int has_checkout = 0;
+    public boolean has_checkin;
+    public boolean has_checkout;
 
     public Bookings() {}
 }
